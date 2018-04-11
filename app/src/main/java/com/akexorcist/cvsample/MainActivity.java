c04d521af32d49b8d5628974028344792e91a1cf
Nicolas Diaz <ndiaz@netlinux.cl>
2018-04-11 13:31:04 -0300

    Version prototipo, abre la camara y analiza la foto tomada mostrando el texto identificado y permitiendo copiar este al portapapeles

---
+++ app/src/main/java/com/akexorcist/cvsample/MainActivity.java
@@ -0,0 +1,367 @@
+/*
+Copyright 2016 Akexorcist
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+package com.akexorcist.cvsample;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.akexorcist.cloudvision.CVRequest;
+import com.akexorcist.cloudvision.CVResponse;
+import com.akexorcist.cloudvision.CloudVision;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity implements CloudVision.Callback {
+    private final static String apiKey = "AIzaSyBIiNrqWaxjZJwub6JHxvmxMYhfbZc7_qM";
+
+    private ImageView imageContainer;
+    private TextView txtTextDetected;
+    private ListView lvLabel;
+    private ProgressBar pbLabel;
+    private static final int TAKE_PICTURE = 1;
+    private Uri imageUri;
+    private String originalRoute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageContainer = (ImageView) findViewById(R.id.image_container);
+        txtTextDetected = (TextView) findViewById(R.id.textDetected);
+        lvLabel = (ListView) findViewById(R.id.lv_label);
+        pbLabel = (ProgressBar) findViewById(R.id.pb_label);
+
+        txtTextDetected.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                saveToClipboard("OCR", txtTextDetected.getText().toString());
+                return true;
+            }
+        });
+
+        this.getPermission();
+        takePhoto();
+    }
+
+    private void saveToClipboard(String key, String text) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(key, text);
+        clipboard.setPrimaryClip(clip);
+    }
+
+
+    public void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        originalRoute = Environment.getExternalStorageDirectory() + "/Pic.jpg";
+
+        File photo = new File(originalRoute);
+        //String reducedpath = compressImage(originalRoute);
+        //File reducedpicture = new File(reducedpath);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    private void startDetect(Bitmap bitmap) {
+        Log.d("NICO", "Inicio de detección");
+        String data = CloudVision.convertBitmapToBase64String(bitmap);
+        CVRequest request = createCVRequest(data);
+        CloudVision.runImageDetection(apiKey, request, this);
+    }
+
+    private CVRequest createCVRequest(String data) {
+        CVRequest.Image image = new CVRequest.Image(data);
+        CVRequest.Feature featureLabelDetection = new CVRequest.Feature(CVRequest.FeatureType.LABEL_DETECTION, 5);
+        CVRequest.Feature featureTextDetected = new CVRequest.Feature(CVRequest.FeatureType.TEXT_DETECTION, 5);
+        List<CVRequest.Feature> featureList = new ArrayList<>();
+        featureList.add(featureLabelDetection);
+        featureList.add(featureTextDetected);
+        List<CVRequest.Request> requestList = new ArrayList<>();
+        requestList.add(new CVRequest.Request(image, featureList));
+        return new CVRequest(requestList);
+    }
+
+    private void setCVResponse(CVResponse cvResponse) {
+        if (cvResponse != null && cvResponse.isResponsesAvailable()) {
+            CVResponse.Response response = cvResponse.getResponse(0);
+            if (response.isLabelAvailable()) {
+                LabelAdapter adapter = new LabelAdapter(response.getLabels());
+                lvLabel.setAdapter(adapter);
+                hideLoading();
+
+                String s = response.getTexts().get(0).getDescription();
+                txtTextDetected.setText(s);
+
+                Log.d("NICO", "FIN de reconocimiento");
+            }
+        }
+    }
+
+    public void showLoading() {
+        lvLabel.setVisibility(View.GONE);
+        pbLabel.setVisibility(View.VISIBLE);
+    }
+
+    public void hideLoading() {
+        lvLabel.setVisibility(View.VISIBLE);
+        pbLabel.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onImageDetectionSuccess(boolean isSuccess, int statusCode, Headers headers, CVResponse cvResponse) {
+        setCVResponse(cvResponse);
+    }
+
+    @Override
+    public void onImageDetectionFailure(Throwable t) {
+        // Do something
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == Activity.RESULT_OK) {
+
+                    String reduced = compressImage(imageUri.getPath());
+                    File reducedphoto = new File(reduced);
+
+                    Uri selectedImage = Uri.fromFile(reducedphoto);
+
+                    getContentResolver().notifyChange(selectedImage, null);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                        imageContainer.setImageBitmap(bitmap);
+
+                        showLoading();
+                        startDetect(bitmap);
+
+                    } catch (Exception e) {
+                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                        Log.e("Camera", e.toString());
+                        hideLoading();
+                    }
+                }
+        }
+    }
+
+    //reduce el tamaño de la imagen
+    public String compressImage(String imageUri) {
+
+        String filePath = getRealPathFromURI(imageUri);
+        Bitmap scaledBitmap = null;
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+//      by setting this field as true, the actual bitmap pixels are not loaded in the memory. Just the bounds are loaded. If
+//      you try the use the bitmap here, you will get null.
+        options.inJustDecodeBounds = true;
+        Bitmap bmp = BitmapFactory.decodeFile(filePath, options);
+
+        int actualHeight = options.outHeight;
+        int actualWidth = options.outWidth;
+
+//      max Height and width values of the compressed image is taken as 816x612
+
+        float maxHeight = 816.0f;
+        float maxWidth = 612.0f;
+        float imgRatio = actualWidth / actualHeight;
+        float maxRatio = maxWidth / maxHeight;
+
+//      width and height values are set maintaining the aspect ratio of the image
+
+        if (actualHeight > maxHeight || actualWidth > maxWidth) {
+            if (imgRatio < maxRatio) {
+                imgRatio = maxHeight / actualHeight;
+                actualWidth = (int) (imgRatio * actualWidth);
+                actualHeight = (int) maxHeight;
+            } else if (imgRatio > maxRatio) {
+                imgRatio = maxWidth / actualWidth;
+                actualHeight = (int) (imgRatio * actualHeight);
+                actualWidth = (int) maxWidth;
+            } else {
+                actualHeight = (int) maxHeight;
+                actualWidth = (int) maxWidth;
+
+            }
+        }
+
+//      setting inSampleSize value allows to load a scaled down version of the original image
+
+        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
+
+//      inJustDecodeBounds set to false to load the actual bitmap
+        options.inJustDecodeBounds = false;
+
+//      this options allow android to claim the bitmap memory if it runs low on memory
+        options.inPurgeable = true;
+        options.inInputShareable = true;
+        options.inTempStorage = new byte[16 * 1024];
+
+        try {
+//          load the bitmap from its path
+            bmp = BitmapFactory.decodeFile(filePath, options);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+
+        }
+        try {
+            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+        }
+
+        float ratioX = actualWidth / (float) options.outWidth;
+        float ratioY = actualHeight / (float) options.outHeight;
+        float middleX = actualWidth / 2.0f;
+        float middleY = actualHeight / 2.0f;
+
+        Matrix scaleMatrix = new Matrix();
+        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+        Canvas canvas = new Canvas(scaledBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+//      check the rotation of the image and display it properly
+        ExifInterface exif;
+        try {
+            exif = new ExifInterface(filePath);
+
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+            Log.d("EXIF", "Exif: " + orientation);
+            Matrix matrix = new Matrix();
+            if (orientation == 6) {
+                matrix.postRotate(90);
+                Log.d("EXIF", "Exif: " + orientation);
+            } else if (orientation == 3) {
+                matrix.postRotate(180);
+                Log.d("EXIF", "Exif: " + orientation);
+            } else if (orientation == 8) {
+                matrix.postRotate(270);
+                Log.d("EXIF", "Exif: " + orientation);
+            }
+            scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0,
+                    scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix,
+                    true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream out = null;
+        String filename = getFilename();
+        try {
+            out = new FileOutputStream(filename);
+
+//          write the compressed bitmap at the destination specified by filename.
+            scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return filename;
+
+    }
+
+    public String getFilename() {
+        File file = new File(Environment.getExternalStorageDirectory().getPath(), "MyFolder/Images");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String uriSting = (file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg");
+        return uriSting;
+
+    }
+
+    private String getRealPathFromURI(String contentURI) {
+        Uri contentUri = Uri.parse(contentURI);
+        Cursor cursor = getContentResolver().query(contentUri, null, null, null, null);
+        if (cursor == null) {
+            return contentUri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(index);
+        }
+    }
+
+    public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        final float totalPixels = width * height;
+        final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+            inSampleSize++;
+        }
+
+        return inSampleSize;
+    }
+
+    private void getPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE,}, 1);
+            }
+        }
+    }
+}
